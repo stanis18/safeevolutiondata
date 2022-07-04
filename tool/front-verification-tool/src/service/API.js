4756f4c627dca6de34a8a7714b520100d46d3e8a
Juliandson <estanislau18@hotmail.com>
2022-07-04 11:14:03 -0300

    Changes docker file

--- tool/front-verification-tool/src/service/API.js
+++ tool/front-verification-tool/src/service/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: "http://34.201.1.6:8000/",
+	baseURL: "http://localhost:8000/",
 	headers: {
 		'Content-Type': 'application/json;charset=utf-8',
 		'Accept': 'application/json'
